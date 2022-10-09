56f2cf9698b7c017124c3afd965b4431532951da
IJ58LG <patryk.kwasek@ing.com>
2022-10-09 19:44:11 +0200

    Added input to set offset

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [countOffset, setCountOffset] = useState<number>(10);
 
   const increment = () => {
-    setCount(count + 1);
+    setCount(count + countOffset);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    setCount(count - countOffset);
   };
 
   const reset = () => {
     setCount(0);
   };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCountOffset(Number(e.target.value));
+  }
 
   return (
     <div className='container'>
@@ -23,9 +28,37 @@ function App() {
       <p className='count'>{count}</p>
 
       <div className='buttons-container'>
-        <button onClick={increment} className='button increment'>Increment</button>
-        <button onClick={decrement} className='button decrement'>Decrement</button>
-        <button onClick={reset} className='button reset'>Reset</button>
+        <button 
+          onClick={increment} 
+          className='button increment'
+        >
+          Increment
+        </button>
+
+        <button 
+          onClick={decrement} 
+          className='button decrement'
+        >
+          Decrement
+        </button>
+        
+        <button 
+          onClick={reset} 
+          className='button reset'
+        >
+          Reset
+        </button>
+      </div>
+
+      <div className='input-container'>
+        <label className='label'>Offset:</label>
+        <input 
+        name='count-number'
+        type='number'
+        defaultValue={countOffset}
+        onChange={handleInput}
+        className='input'
+      />
       </div>
     </div>
   )
